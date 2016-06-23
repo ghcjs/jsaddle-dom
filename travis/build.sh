5b13f53740a7189f0131dcdff2b6fbfb69a5d5b6
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2016-06-23 14:51:55 +1200

    Update travis build scripts

---
+++ travis/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ex
+
+echo $PATH
+export LC_ALL=C.UTF-8
+
+if [[ -d .cabal && -d .ghc ]]; then
+    cp -a .cabal .ghc /root
+fi
+
+cabal update
+cabal new-build
+
+# update the cache
+rm -rf .cabal
+cp -a /root/.cabal ./
+rm -rf .ghc
+cp -a /root/.ghc ./
