85b1f1f0235d38648e716875d88c29729bd47386
Hamish Mackenzie <Hamish.K.Mackenzie@googlemail.com>
2017-05-18 12:11:20 +1200

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 }:
 mkDerivation {
   pname = "jsaddle-dom";
-  version = "0.7.1.0";
+  version = "0.9.0.0";
   src = builtins.filterSource (path: type: !(builtins.elem (baseNameOf path) [ ".git" "dist" ])) ./.;
   libraryHaskellDepends = [
     base base-compat jsaddle lens text transformers
